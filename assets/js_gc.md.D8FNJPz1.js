1bee3d222ab43eb795e56008fc61a512144a1640
wolf-sence <50403015+wolf-sence@users.noreply.github.com>
2025-02-08 02:44:23 +0000

    Deploying to static-pages from @ wolf-sence/wolf-sence.github.io@44688269424e4bb50ab45ea051f947d359978ac8 🚀

---
+++ assets/js_gc.md.D8FNJPz1.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o as a}from"./chunks/framework.BmLMQRXF.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"js/gc.md","filePath":"js/gc.md","lastUpdated":1738981284000}'),s={name:"js/gc.md"};function c(r,o,n,d,p,_){return a(),t("div")}const f=e(s,[["render",c]]);export{m as __pageData,f as default};
